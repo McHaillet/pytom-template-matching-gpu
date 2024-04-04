2d5e7001d562bdb50006e4de506c863131c567a9
McHaillet <martenchaillet@gmail.com>
2024-04-04 23:47:37 +0200

    write mean and std volumes

--- src/pytom_tm/tmjob.py
+++ src/pytom_tm/tmjob.py
@@ -650,8 +650,10 @@ class TMJob:
         angle_volume = results[1][:self.search_size[0], :self.search_size[1], :self.search_size[2]]
         mean = results[2] / len(angle_ids)
         std = np.sqrt((results[3] - (results[2] ** 2 / len(angle_ids))) / (len(angle_ids) - 1))
-        write_mrc(self.output_dir.joinpath(f'{self.tomo_id}_scores_mean.mrc'), results[2], self.voxel_size)
-        write_mrc(self.output_dir.joinpath(f'{self.tomo_id}_scores_std.mrc'), results[3], self.voxel_size)
+        write_mrc(self.output_dir.joinpath(f'{self.tomo_id}_scores_mean.mrc'), mean,
+                  self.voxel_size)
+        write_mrc(self.output_dir.joinpath(f'{self.tomo_id}_scores_std.mrc'), std,
+                  self.voxel_size)
         self.job_stats = results[4]
 
         del tm  # delete the template matching plan
